6c327b858728ca02db24b825aca44e9b218f5cbb
VtyurinaSI <sveta@WIN-QF9MU8QVJJQ>
2025-10-20 11:57:31 +0300

    отладка

--- CompilerSevice/CodeExample.cs
+++ CompilerSevice/CodeExample.cs
@@ -1,14 +1,5 @@
 ﻿/*
 
-using System;
-class Program
-{
-    static void Main()
-    {
-        int number = 5;
-        Console.WriteLine("The number is: " + number); 
-        string unusedVariable = "This variable is not used";
-    }
-}
+using System;class Program{static void Main(){ int number = 5; Console.WriteLine(number+q); int unusedVariable = 7;}}
 */
 
--- docker/init/00-create-dbs.sh
+++ docker/init/00-create-dbs.sh
@@ -1,1 +1,17 @@
-#!/bin/shset -eux: "${POSTGRES_USER:=postgres}"echo ">> creating databases if missing"createdb -U "$POSTGRES_USER" AuthService   2>/dev/null || truecreatedb -U "$POSTGRES_USER" UserProgress  2>/dev/null || truecreatedb -U "$POSTGRES_USER" PatternsMinIO 2>/dev/null || trueecho ">> applying schema to UserProgress"psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d UserProgress \     -f /docker-entrypoint-initdb.d/20-userprogress-db.psqlecho ">> applying schema: PatternsMinIO"psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d PatternsMinIO -f /docker-entrypoint-initdb.d/20-patternsminio-db.psqlecho ">> done"
+set -eux
+: "${POSTGRES_USER:=postgres}"
+
+echo ">> creating databases if missing"
+createdb -U "$POSTGRES_USER" AuthService   2>/dev/null || true
+createdb -U "$POSTGRES_USER" UserProgress  2>/dev/null || true
+createdb -U "$POSTGRES_USER" PatternsMinIO 2>/dev/null || true
+
+echo ">> applying schema to UserProgress"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d UserProgress \
+     -f /docker-entrypoint-initdb.d/20-userprogress-db.psql
+
+echo ">> applying schema: PatternsMinIO"
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d PatternsMinIO -f /docker-entrypoint-initdb.d/20-patternsminio-db.psql
+
+
+echo ">> done"
