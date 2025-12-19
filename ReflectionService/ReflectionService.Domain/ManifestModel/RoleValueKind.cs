31fd6619a3bd56c18b1161f043f168ab9151fc0c
VtyurinaSI <sveta@WIN-QF9MU8QVJJQ>
2025-12-19 16:36:16 +0300

    .

--- ReflectionService/ReflectionService.Domain/ManifestModel/RoleValueKind.cs
+++ ReflectionService/ReflectionService.Domain/ManifestModel/RoleValueKind.cs
@@ -1,9 +1,5 @@
 namespace ReflectionService.Domain.ManifestModel;
 
-/// <summary>
-/// What kind of value a step stores into a role.
-/// Mostly for documentation/validation.
-/// </summary>
 public enum RoleValueKind
 {
     None,
