96cc54ea46c9d724c7a5c4ce70738027eba62559
VtyurinaSI <sveta@WIN-QF9MU8QVJJQ>
2026-01-22 16:20:49 +0300

    .

---
+++ Orchestrator/Orchestrator.Application/Consumers/WrongProjectStructureConsumer.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+using SmartLearning.Contracts;
+
+namespace Orchestrator.Application.Consumers
+{
+    public class WrongProjectStructureConsumer : IConsumer<WrongProjectStructure>
+    {
+        private readonly CompletionHub _hub;
+        public WrongProjectStructureConsumer(CompletionHub hub) => _hub = hub;
+
+        public Task Consume(ConsumeContext<WrongProjectStructure> ctx)
+        {
+            _hub.SetCompleted(ctx.Message.CorrelationId, false, ctx.Message.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
+
